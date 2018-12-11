d3a7582277b423a272f7d9e18068d563fd669a13
luis doriz <ldorizsalazar@gmail.com>
2018-12-10 21:03:01 -0600

    migrate

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://luisdoriz98:roble219@ds147723.mlab.com:47723/devconnector_course"
+    "mongodb://luisdoriz98:roble219@ds147723.mlab.com:47723/devconnector_course",
+  secretOrKey: "secret"
 };
